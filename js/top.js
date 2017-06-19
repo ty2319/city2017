53fb1cc326cf5f627bebf8510e69ac5d4d16838f
ty2319 <ty2319@hotmail.com>
2017-06-19 17:51:27 +0900

    end

--- js/top.js
+++ js/top.js
@@ -28,44 +28,43 @@ $(function() {
 	
 	// 使用例
 	$.getJSON('/symbol/hp/baseball/games/2017/city/js/update.json').done(function(json, status, request) {
-			$(json).each(function(i, data) {
+		$(json).each(function(i, data) {
 				
-				var today	= new Date( $.now() ); // 今日の日付を取得
-				var ua = navigator.userAgent.toLowerCase();
-	 
-				// iPhone
-				var isiPhone = (ua.indexOf('iphone') > -1);
-				// iPad
-				var isiPad = (ua.indexOf('ipad') > -1);
-				// iPod
-				var isiPod = (ua.indexOf('ipod') > -1);
-				// Android
-				var isAndroid = (ua.indexOf('android') > -1) && (ua.indexOf('mobile') > -1);
-				// Android Tablet
-				var isAndroidTablet = (ua.indexOf('android') > -1) && (ua.indexOf('mobile') == -1);
+			var today	= new Date( $.now() ); // 今日の日付を取得
+			var ua = navigator.userAgent.toLowerCase();
+	
+			// iPhone
+			var isiPhone = (ua.indexOf('iphone') > -1);
+			// iPad
+			var isiPad = (ua.indexOf('ipad') > -1);
+			// iPod
+			var isiPod = (ua.indexOf('ipod') > -1);
+			// Android
+			var isAndroid = (ua.indexOf('android') > -1) && (ua.indexOf('mobile') > -1);
+			// Android Tablet
+			var isAndroidTablet = (ua.indexOf('android') > -1) && (ua.indexOf('mobile') == -1);
 				
-				var elem	= '.' + data.class, // class
-					date	= new Date( data.date ), // date
-					ago		= date.setDate(date.getDate() + 1); // 更新日 + 1日
-		
-				if (elem == '.top' && today < ago && !isiPhone && !isiPad && !isiPod) { // 今日(today)がago(更新日 + 1日)より前なら
-						
-					Push.Permission.request();
+			var elem	= '.' + data.class, // class
+				date	= new Date( data.date ), // date
+				ago		= date.setDate(date.getDate() + 1); // 更新日 + 1日
+	
+			if (elem == '.top' && today < ago && !isiPhone && !isiPad && !isiPod) { // 今日(today)がago(更新日 + 1日)より前なら
 					
-					Push.create('都市対抗特設サイト', {
-					　　body	: '都市対抗特設サイトを公開しました。',
-					　　icon	: 'icon.gif',
-					　　timeout	: 8000, // 通知が消えるタイミング
-					　　vibrate	: [100, 100, 100], // モバイル端末でのバイブレーション秒数
-					　　onClick	: function() {
-					　　　　// 通知がクリックされた場合の設定
-					　　　　console.log(this);
-					　　}
-					});
-				}
+				Push.Permission.request();
+				
+				Push.create('都市対抗特設サイト', {
+				　　body	: 'マスコットのご紹介を公開しました。',
+				　　icon	: 'icon.gif',
+				　　timeout	: 8000, // 通知が消えるタイミング
+				　　vibrate	: [100, 100, 100], // モバイル端末でのバイブレーション秒数
+				　　onClick	: function() {
+				　　　　window.open('http://http://www.ntt-west.co.jp/symbol/hp/baseball/games/2017/city/mascot.html');
+				　　}
+				});
+			}
 		
-			});
 		});
+	});
 });
 
 $(window).on('load resize' , function() {
