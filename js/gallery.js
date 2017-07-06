1ff454e52754e006d88910cab0f4bf1522f2344d
ty2319 <ty2319@hotmail.com>
2017-07-06 19:00:39 +0900

    gallery end

--- js/gallery.js
+++ js/gallery.js
@@ -1,9 +1,13 @@
 $(document).ready(function(){
 	
+	if ($(window).width() < 600) {
+		$('.accordion').hide();
+	}
+	
 	$('.accordion').css('cursor','pointer');	
 	$('.accordion + div').hide().css('margin-bottom','10em');
 	
-	$('.accordion').click(function(){
+	$('.accordion').on('click', function(){
 		$(this).next('div').slideToggle('slow');
 	}).toggle(function() {
 		$('span' , this).text("※クリックすると上に閉じます。 ▲");
@@ -12,10 +16,12 @@ $(document).ready(function(){
 	});
 	
 	$("a[rel=game1],a[rel=game2],a[rel=game3],a[rel=game4],a[rel=game5]").fancybox({
-		'transitionIn'		: 'fade',
-		'transitionOut'		: 'fade',
-		'titlePosition' 	: 'over',
-		'titleFormat'		: function(title, currentArray, currentIndex, currentOpts) {
+		'padding'		: 0,
+		'margin'		: 20,
+		'transitionIn'	: 'elastic',
+		'transitionOut'	: 'elastic',
+		'titlePosition'	: 'over',
+		'titleFormat'	: function(title, currentArray, currentIndex, currentOpts) {
 			return '<span id="fancybox-title-over">Image ' + (currentIndex + 1) + ' / ' + currentArray.length + (title.length ? ' &nbsp; ' + title : '') + '</span>';
 		}
 	});
@@ -23,10 +29,7 @@ $(document).ready(function(){
 	$('.fancy dt + dd').css('margin',0);
 	$('.fancy > dt:first-child').css({'border':'none','margin-top': 0,'padding-top': '1em'});	
 	
-	$(".light-gallery").lightGallery({
-        showThumbByDefault:true,
-        addClass:'showThumbByDefault',
-		loop:true
-	});
+	$('.half > ul').photobox('a', {thumbs:true});
+	$('.half > ul li').height($('.half > ul li').width());
 	
 }); 
